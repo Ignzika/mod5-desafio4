32da43e0390581ae2b4b220820f0c3d8de49919d
Ignacio Bustos <ark.zikael@gmail.com>
2023-10-21 03:16:32 -0300

    home img restored

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         </h3>
       </div>
       <div className="imgHomeContainer">
-        <img src="src\assets\img\aipizza2.jpg" alt="pizzaWelcome" />
+        <img src="src/assets/img/aipizza2.jpg" alt="pizzaWelcome" />
       </div>
       <hr className="rpgui-container framed-golden-2" />
     </div>
